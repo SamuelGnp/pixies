093d528ddee6ac4c8dbc82fae7e55e3ef8997249
SamuelGnp <snowhowhero@gmail.com>
2022-10-07 21:22:10 +0100

    finilised

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,18 +3,34 @@ import { Carousel } from 'react-bootstrap';
 import Axios from 'axios';
 
 const App = () => {
-  const[items,setItems]=useState([]);
-
-  useEffect(() => {
-    const url = 'http://localhost:9090/api/';
+  const[items,setItems2]=useState([]);
+  useEffect(
+    () => {  
+    const url = window.settings.backendUrl;
     Axios.get(url)
       .then(response => {
-        setItems(response.data);
+        setItems2(response.data);
       })
       .catch(error => {
         console.log(error);
       });
   }, []);
-}
-
+  return (
+    <div className='container'>
+      <div className='row'>
+        <Carousel>
+          {items.map(item => (
+            <Carousel.Item>
+              <img className='d-block w-100' src={item.url} alt='' />
+              <Carousel.Caption>
+                <h3>{item.caption}</h3>
+                <p>{item.details}</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+          ))}
+        </Carousel>
+      </div>
+    </div>
+  );
+};
 export default App;
--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path"); 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: "./src/index.js", 
@@ -28,7 +29,12 @@ module.exports = {
   },
 
   plugins: [new HtmlWebpackPlugin({
-        template: "./src/index.html", 
-  }),
-            new CleanWebpackPlugin()]
+              template: "./src/index.html",}),
+            new CleanWebpackPlugin(),
+            new CopyPlugin({
+              patterns: [
+                {from: 'static', to: '.'},
+              ],
+            }),
+          ]
 };
